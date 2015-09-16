e68e21b3847d0838ca6c2bfa94d354f4c121b723
davidamin <amin.david.m@gmail.com>
2015-09-16 00:38:44 -0400

    Boom! We have a project

---
+++ android/src/com/virginia/cs/SlingshotSam/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.virginia.cs.SlingshotSam.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.virginia.cs.SlingshotSam.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/virginia/cs/SlingshotSam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.virginia.cs.SlingshotSam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.virginia.cs.SlingshotSam.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
