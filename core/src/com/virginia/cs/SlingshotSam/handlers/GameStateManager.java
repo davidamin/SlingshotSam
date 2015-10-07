5da5e12c497ab8a8f2c3b9d747261d442454fb80
macsochor <macleansochor@gmail.com>
2015-10-07 15:50:17 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/src/com/virginia/cs/SlingshotSam/Game.java

---
+++ core/src/com/virginia/cs/SlingshotSam/handlers/GameStateManager.java
@@ -0,0 +1,55 @@
+package com.virginia.cs.SlingshotSam.handlers;
+
+/**
+ * Created by Mac on 9/27/2015.
+ */
+
+
+import java.util.Stack;
+
+import com.virginia.cs.SlingshotSam.main.Game;
+import com.virginia.cs.SlingshotSam.states.GameState;
+import com.virginia.cs.SlingshotSam.states.Play;
+import java.util.Stack;
+
+public class GameStateManager {
+    private Game game;
+    private Stack<GameState> gameStates;
+    public static final int PLAY = 912837;
+
+    public GameStateManager(Game game) {
+        this.game = game;
+        this.gameStates = new Stack();
+        this.pushState(912837);
+    }
+
+    public Game game() {
+        return this.game;
+    }
+
+    public void update(float dt) {
+        ((GameState)this.gameStates.peek()).update(dt);
+    }
+
+    public void render() {
+        ((GameState)this.gameStates.peek()).render();
+    }
+
+    private GameState getState(int state) {
+        return state == 912837?new Play(this):null;
+    }
+
+    public void setState(int state) {
+        this.popState();
+        this.pushState(state);
+    }
+
+    public void pushState(int state) {
+        this.gameStates.push(this.getState(state));
+    }
+
+    public void popState() {
+        GameState g = (GameState)this.gameStates.pop();
+        g.dispose();
+    }
+}
---
+++ core/src/com/virginia/cs/SlingshotSam/states/Play.java
@@ -0,0 +1,78 @@
+package com.virginia.cs.SlingshotSam.states;
+
+/**
+ * Created by Mac on 9/27/2015.
+ */
+
+    import com.badlogic.gdx.Gdx;
+    import com.badlogic.gdx.graphics.OrthographicCamera;
+    import com.badlogic.gdx.math.Vector2;
+    import com.badlogic.gdx.physics.box2d.Body;
+    import com.badlogic.gdx.physics.box2d.BodyDef;
+    import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+    import com.badlogic.gdx.physics.box2d.CircleShape;
+    import com.badlogic.gdx.physics.box2d.FixtureDef;
+    import com.badlogic.gdx.physics.box2d.PolygonShape;
+    import com.badlogic.gdx.physics.box2d.World;
+    import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+    import com.virginia.cs.SlingshotSam.handlers.GameStateManager;
+    import com.virginia.cs.SlingshotSam.handlers.MyContactListener;
+    import com.virginia.cs.SlingshotSam.states.GameState;
+
+public class Play extends GameState {
+    private World world = new World(new Vector2(0.0F, -2.81F), true);
+    private Box2DDebugRenderer b2dr;
+    private OrthographicCamera b2dCam;
+
+    public Play(GameStateManager gsm) {
+        super(gsm);
+        this.world.setContactListener(new MyContactListener());
+        this.b2dr = new Box2DDebugRenderer();
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(1.6F, 1.2F);
+        bdef.type = BodyType.StaticBody;
+        Body body = this.world.createBody(bdef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(1.5F, 0.05F);
+        FixtureDef fdef = new FixtureDef();
+        fdef.shape = shape;
+        fdef.filter.categoryBits = 2;
+        fdef.filter.maskBits = 12;
+        body.createFixture(fdef).setUserData("ground");
+        bdef.position.set(1.6F, 2.0F);
+        bdef.type = BodyType.DynamicBody;
+        body = this.world.createBody(bdef);
+        shape.setAsBox(0.05F, 0.05F);
+        fdef.shape = shape;
+        fdef.filter.categoryBits = 4;
+        fdef.filter.maskBits = 2;
+        body.createFixture(fdef).setUserData("box");
+        bdef.position.set(1.53F, 2.2F);
+        body.applyForceToCenter(10,10,true);
+        body = this.world.createBody(bdef);
+        CircleShape cshape = new CircleShape();
+        cshape.setRadius(0.05F);
+        fdef.shape = cshape;
+        fdef.filter.categoryBits = 8;
+        fdef.filter.maskBits = 2;
+        body.createFixture(fdef).setUserData("ball");
+        this.b2dCam = new OrthographicCamera();
+        this.b2dCam.setToOrtho(false, 3.2F, 2.4F);
+
+    }
+
+    public void handleInput() {
+    }
+
+    public void update(float dt) {
+        this.world.step(dt, 6, 2);
+    }
+
+    public void render() {
+        Gdx.gl20.glClear(16384);
+        this.b2dr.render(this.world, this.b2dCam.combined);
+    }
+
+    public void dispose() {
+    }
+}
