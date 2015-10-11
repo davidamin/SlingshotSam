4eb6e6f6666e3905b2a275a934d95684410d9ed8
imjustjoshua <Joshua+github@royhousehold.net>
2015-10-11 12:51:19 -0400

    TileTest
    I decided to go the extra mile
    and made a new map with Tiled
    I'm not an artist,
    But I tried my hardest
    and the work I've done makes me smile
    
    But if you don't like my map
    use AndroidLauncher to dodge that crap
    take your time
    comment the right line
    and the right file will drop into your lap
    
    If you run the TileTest on your phone
    You'll have access to features that won't make you moan
    You can pan around on the screen
    (use your finger - you know what I mean)
    Take a good look at the whole zone!

--- android/src/com/virginia/cs/SlingshotSam/android/AndroidLauncher.java
+++ android/src/com/virginia/cs/SlingshotSam/android/AndroidLauncher.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.virginia.cs.SlingshotSam.Game;
+import com.virginia.cs.SlingshotSam.TileTest;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		//Use initialize Game for running the actual game, use initialize TileTest for the view of practice map
 		initialize(new Game(), config);
+		//initialize(new TileTest(), config);
+
 	}
 }
